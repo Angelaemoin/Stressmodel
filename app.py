f0ee85047b8adead11143c995a8f5fec26590993
Harshal Baid <68075910+Harshalbaid12@users.noreply.github.com>
2022-11-01 12:17:42 +0530

    some changes

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from re import DEBUG
 import numpy as np
 from flask import Flask,render_template,request
 import pickle
@@ -13,29 +14,26 @@ def test():
     return render_template('test.html')
 
 @app.route("/predict",methods=['GET','POST'])
-# q1 = request.args['Q1']
-# def predict():
-#    return render_template("predict.html",output=q1)
+
 def prediction():
     if(request.method == 'POST'):
-        q1 = int(request.form['Q1'])
-        q2 = int(request.form['Q2'])
-        q3 = int(request.form['Q3'])
-        q4 = int(request.form['Q4'])
-        q5 = int(request.form['Q5'])
-        q6 = int(request.form['Q6'])
-        q7 = int(request.form['Q7'])
-        allparams = [q1,q2,q3,q4,q5,q6,q7]
+        nervousness = int(request.form['Q1'])
+        control = int(request.form['Q2'])
+        Worry = int(request.form['Q3'])
+        relaxation = int(request.form['Q4'])
+        restlessness = int(request.form['Q5'])
+        irritable = int(request.form['Q6'])
+        fear = int(request.form['Q7'])
+        name = request.form["name"]
+        allparams = [nervousness,control,Worry,relaxation,restlessness,irritable,fear]
         print(allparams)
         array = np.array(allparams)
         output = model.predict([array]).tolist()
-        print(output)
-
-
+        level = output[0]
        
-    # return (model.predict([[3,3,3,3,3,3,3]]).tolist())
-    return (render_template("predict.html",output = output) )
+        print(output)
+    return (render_template("predict.html",output = level,name = name ))
     
 
 if __name__ == '__main__':
-    app.run(debug==True)
+    app.run(DEBUG==True)
