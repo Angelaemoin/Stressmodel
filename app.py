d1d2dbd58ede10c99f888e7750986d6b8e22af39
Harshal Baid <68075910+Harshalbaid12@users.noreply.github.com>
2022-11-15 19:28:38 +0530

    built model changed suggestion

--- app.py
+++ app.py
@@ -16,23 +16,38 @@ def test():
 @app.route("/predict",methods=['GET','POST'])
 
 def prediction():
+
     if(request.method == 'POST'):
-        nervousness = int(request.form['Q1'])
-        control = int(request.form['Q2'])
-        Worry = int(request.form['Q3'])
-        relaxation = int(request.form['Q4'])
-        restlessness = int(request.form['Q5'])
-        irritable = int(request.form['Q6'])
-        fear = int(request.form['Q7'])
-        name = request.form["name"]
-        allparams = [nervousness,control,Worry,relaxation,restlessness,irritable,fear]
+        q1 = int(request.form['Q1'])
+        q2 = int(request.form['Q2'])
+        q3 = int(request.form['Q3'])
+        q4 = int(request.form['Q4'])
+        q5 = int(request.form['Q5'])
+        q6 = int(request.form['Q6'])
+        q7 = int(request.form['Q7'])
+        q8 = int(request.form["Q8"])
+        q9 = int(request.form["Q9"])
+        q10 = int(request.form["Q10"])
+        q11 = int(request.form["Q11"])
+        q12 = int(request.form["Q12"])
+        q13 = int(request.form["Q13"])
+        q14 = int(request.form["Q14"])
+        q15 = int(request.form["Q15"])
+        q16 = int(request.form["Q16"])
+        q17 = int(request.form["Q17"])
+        q18 = int(request.form["Q18"])
+        q19 = int(request.form["Q19"])
+        q20 = int(request.form["Q20"])
+        q21 = int(request.form["Q21"])
+
+
+        allparams = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21]
         print(allparams)
         array = np.array(allparams)
         output = model.predict([array]).tolist()
         level = output[0]
-       
-        print(output)
-    return (render_template("predict.html",output = level,name = name ))
+        print(output[0])
+    return (render_template("predict.html",leveloutput = level))
     
 
 if __name__ == '__main__':
